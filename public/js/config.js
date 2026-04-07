f6718ab46d5b5f0dbb130f0da755272c1e443845
itsTeddy <itsteddy@zohomail.eu>
2026-04-07 11:42:15 +0200

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 export const CONFIG = {
     AUTH_URL: "https://auth.sposlearning.cz",
     API_URL: "https://api.spolearning.cz",
-    BASE_URL: "https://www.sposlearning.cz",
-    ADMIN_URL: "https://admin.sposlearning.cz",
+    BASE_URL: "https://dev.sposlearning.cz",
+    ADMIN_URL: "https://dev-admin.sposlearning.cz",
     SERVER_URL: "https://pve.sposlearning.cz",
     NAS_URL: "https://nas.spolearning.cz",
 }
