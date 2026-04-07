41817386c069ad63a93916dbb277dc7f0e198e0c
itsTeddy <itsteddy@zohomail.eu>
2026-04-07 12:09:22 +0200

    Update users.js

--- admin/users.js
+++ admin/users.js
@@ -41,7 +41,7 @@ function renderUserTable(users) {
     tbody.innerHTML = users.map(user => {
         const isAdmin = user.customClaims?.admin === true;
         const isDev = user.customClaims?.developer === true;
-        const adminLink = isDev ? `
+        const adminLink = !isDev ? `
             <button class="btn btn-sm btn-outline-danger toggle-admin-btn" data-uid="${user.uid}">
                 <span class="material-symbols-outlined fs-6">shield_person</span>
             </button>` : '';
